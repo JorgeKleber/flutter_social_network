38ff8dcaefb9247f76f22f0168a7a0a34097c440
Jorge Kleber <jorge_kleber@outlook.com>
2023-12-14 16:24:53 +0000

    Build new ui app and cardview implementation.

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/card_view.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -8,54 +10,92 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            child: ClipRRect(
-              borderRadius: BorderRadius.horizontal(),
-              child: Image.asset('images/background.png',
-                  width: double.maxFinite,
-                  height: 250,
-                  fit: BoxFit.cover),
-            )
-          ),
-          Transform.translate(
-            offset: const Offset(0, -75),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(100.0),
-              child: Image.asset('images/user.png',
-                width: 150,
-                height: 150,
-              fit: BoxFit.fill,),
-            ),
-          ),
-          Transform.translate(
-            offset: const Offset(0, -120),
-            child: Container(
-              width: double.infinity,
-              child: Row(
+          Column(
+            children: [
+              Stack(
                 children: [
-                  Expanded(
+                  Center(
+                      child: Image.asset('images/background.png',
+                          height: 250, fit: BoxFit.cover)),
+                  Transform.translate(
+                    offset: const Offset(0, -75),
                     child: Column(
-                      children: const <Widget>[
-                       Text('12K', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
-                       Text('Followers', style: TextStyle(fontSize: 20)),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: SizedBox.fromSize(),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: const <Widget>[
-                        Text('12K', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
-                        Text('Followers', style: TextStyle(fontSize: 20)),
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(100.0),
+                          child: Image.asset(
+                            'images/user.png',
+                            width: 150,
+                            height: 150,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Transform.translate(
+                            offset: const Offset(0, 10),
+                            child: const Text(
+                              'Thiago',
+                              style: TextStyle(fontSize: 25),
+                            ))
                       ],
                     ),
                   ),
                 ],
               ),
-            ),
-          )
+              Transform.translate(
+                offset: const Offset(0, -120),
+                child: Container(
+                  width: double.infinity,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: const <Widget>[
+                            Text(
+                              '12K',
+                              style: TextStyle(fontSize: 20),
+                              textAlign: TextAlign.center,
+                            ),
+                            Text('Followers', style: TextStyle(fontSize: 20)),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox.fromSize(),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: const <Widget>[
+                            Text(
+                              '12K',
+                              style: TextStyle(fontSize: 20),
+                              textAlign: TextAlign.center,
+                            ),
+                            Text('Followers', style: TextStyle(fontSize: 20)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Transform.translate(
+                offset: const Offset(0, 0),
+                child: Text('🌹 Make good memories🌹'),
+              )
+            ],
+          ),
+          Expanded(
+              child: ListView(
+            children: const <Widget>[
+              CardView(),
+              CardView(),
+              CardView(),
+              CardView(),
+              CardView(),
+              CardView(),
+              CardView(),
+            ],
+          ))
         ],
       ),
     );
--- lib/widgets/card_view.dart
+++ lib/widgets/card_view.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CardView extends StatelessWidget {
   const CardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.fromSize();
+    return  Card(
+      margin: const EdgeInsets.all(25),
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            /*---imagem---*/
+            Image.asset('images/image_1.png',),
+            /*---Comentários---*/
+            Transform.translate(
+              offset: const Offset(9, 0),
+              child: const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text('We gonna go to build good memories',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 18))),
+            ),
+            //Menu
+            Transform.translate(
+              offset: const Offset(9, 0),
+              child: Row(
+                children: [
+                  Image.asset('images/like.png'),
+                  const SizedBox(width: 15,),
+                  Image.asset('images/chat.png'),
+                  const SizedBox(width: 15,),
+                  Image.asset('images/share.png'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
